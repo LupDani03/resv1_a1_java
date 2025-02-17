b074441a9f98abb74bbe0fdcea706d369ec7a3c0
LupDani03 <daniel.lup@stud.ubbcluj.ro>
2025-02-17 19:27:22 +0200

    hat die Evenimente-Anzahl gemacht

--- src/ubung1.java
+++ src/ubung1.java
@@ -14,7 +14,7 @@ import com.google.gson.reflect.TypeToken;
 
 
 /**
- * Die Klasse {@code Ubung} liest Patientendaten aus einer XML-Datei, verarbeitet diese
+ * Die Klasse {@code Ubung} liest evenimenteendaten aus einer XML-Datei, verarbeitet diese
  * und speichert Statistiken über Krankenhäuser in eine Datei.
  */
 
@@ -41,11 +41,16 @@ public class ubung1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Anfangsbuchstabe: ");
         String ch = sc.nextLine();
+        List<String> noDuplicates = new ArrayList<>();
         for (Evenimente objekt : evenimenteList) {
-            if (objekt.mitgliedsname.charAt(0) == ch.charAt(0)) {
-                System.out.println(objekt.mitgliedsname);
+            if (!noDuplicates.contains(objekt.mitgliedsname)) {
+                if (objekt.mitgliedsname.charAt(0) == ch.charAt(0)) {
+                    System.out.println(objekt.mitgliedsname);
+                }
+                noDuplicates.add(objekt.mitgliedsname);
             }
         }
+
         //2te Aufgabe(Alle Anzeige von die Stark-House)
         // Filtert Evenimente mit dem Haus "Stark" und sortiert nach Datum
         List<Evenimente> evenimenteDaten = new ArrayList<>();
@@ -60,6 +65,40 @@ public class ubung1 {
         System.out.println("\nMitgliedern von den Hause Stark gesortet: ");
         for (Evenimente evenimente : evenimenteDaten) {
             System.out.println(evenimente.datum + " - Ereignis: " + evenimente.ereignis);
+        }
+
+        // Zählt die Anzahl der Evenimente pro haus
+        int eStark = 0;
+        int eLannister = 0;
+        int eTargaryen = 0;
+        int eGreyjoy = 0;
+        int eBaratheon = 0;
+
+        for (Evenimente evenimente : evenimenteList) {
+            if (evenimente.haus.equals("Stark"))
+                eStark++;
+            else if (evenimente.haus.equals("Lannister"))
+                eLannister++;
+            else if (evenimente.haus.equals("Targaryen"))
+                eTargaryen++;
+            else if (evenimente.haus.equals("Greyjoy"))
+                eGreyjoy++;
+            else if (evenimente.haus.equals("Baratheon"))
+                eBaratheon++;
+            else
+                throw new RuntimeException("Haus ist falsch");
+        }
+
+        // Schreibt die Evenimenten Anzahl in eine Datei
+        try {
+            String daten = "Stark#" + eStark + "\n" +
+                    "Lannister#" + eLannister + "\n" +
+                    "Targaryen#" + eTargaryen + "\n" +
+                    "Greyjoy#" + eGreyjoy + "\n" +
+                    "Baratheon#" + eBaratheon;
+            Files.write(Path.of("ergebnis.txt"), daten.getBytes());
+        } catch (IOException e) {
+            System.err.println("Fehler beim Schreiben in die Datei: " + e.getMessage());
         }
     }
 }
